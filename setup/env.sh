52e72f87cc25aa3bb01eb179e4faf8cf4f6b505a
imssyang <imssyang@gmail.com>
2022-11-22 14:34:15 +0800

    add env.sh

--- setup/env.sh
+++ setup/env.sh
@@ -2,12 +2,6 @@
 
 # python2
 export PYTHON2_DISABLE_ENV=yes
-package_paths=(
-  /opt/python2/lib/python2.7/site-packages
-  /opt/python2/sample
-  $PYTHONPATH
-)
-IFS=:$IFS; export PYTHONPATH="${package_paths[*]}"; IFS=${IFS#?}
 eval "optbin -s /opt/python2/bin"
 eval "optpkg -s /opt/python2/lib/pkgconfig"
 eval "optlib -s /opt/python2/lib"
